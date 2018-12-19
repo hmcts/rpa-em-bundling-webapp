4f30875f9bebe69ecd07e3a0055515cf3a54d25b
Velican <duzgunce.vd@gmail.com>
2018-12-19 11:58:13 +0000

    updating angular package

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function (config) {
   config.set({
     basePath: "",
-    frameworks: ["jasmine", "@angular/cli"],
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
@@ -13,21 +13,19 @@ module.exports = function (config) {
       require("karma-coverage-istanbul-reporter"),
       require("karma-htmlfile-reporter"),
       require("karma-mocha-reporter"),
-      require("@angular/cli/plugins/karma")
+      require("@angular-devkit/build-angular/plugins/karma")
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ "html", "lcovonly" ],
+      dir: require('path').join(__dirname, 'coverage'), reports: [ "html", "lcovonly" ],
       fixWebpackSourcePaths: true
     },
     htmlReporter: {
       outputFile: "report/units.html",
     },
-    angularCli: {
-      environment: "dev"
-    },
+    
     reporters: ["html" , "progress", "kjhtml", "mocha"],
     port: 9876,
     colors: true,
